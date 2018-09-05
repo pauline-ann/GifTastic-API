b01b3b59293520d1186a70e56ab2eff9dce72e62
pauline-ann <paulinebantayan@gmail.com>
2018-09-04 20:47:09 -0700

    Restructure page format and add finishing aesthetic touches

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,18 @@
 // Starting array for food types
-var foodTypes = ["pancakes", "ramen", "coffee", "macaroni & cheese", "fries", "tacos"];
+var foodTypes = [
+    "sushi",
+    "eggs benedict",
+    "ramen",
+    "avocado",
+    "grilled cheese",
+    "macaroni & cheese",
+    "bacon",
+    "fries",
+    "tacos",
+    "burrito",
+    "nachos",
+    "pancakes",
+    "brownies"];
 
 // Create a button in the event that the add button is clicked
 $("#add-food").on("click", function (event) {
@@ -16,7 +29,7 @@ function renderButtons() {
 
     for (var i = 0; i < foodTypes.length; i++) {
         var newButton = $("<button>");
-        newButton.addClass("food");
+        newButton.addClass("button");
         newButton.attr("data-type", foodTypes[i]);
         newButton.text(foodTypes[i]);
         $("#buttons-view").append(newButton);
@@ -26,8 +39,9 @@ function renderButtons() {
 // Render initial buttons
 renderButtons();
 
-// Add click event listeners to all elements with "food" class
-$(document).on("click", ".food", displayFood);
+// Add click event listeners to all elements with "button" and "gif" classes
+$(document).on("click", ".button", displayFood);
+$(document).on("click", ".gif", changeState);
 
 // Function for displaying the appropriate content from the API
 function displayFood() {
@@ -45,31 +59,36 @@ function displayFood() {
         // Generate initial still images & rating
         for (var i = 0; i < 10; i++) {
 
+            var foodDiv = $("<div>");
+
             var stillSource = results[i].images.fixed_height_still.url;
             var animatedSource = results[i].images.fixed_height.url;
             var foodImage = $("<img>");
             foodImage.attr("src", stillSource).addClass("gif").attr("data-still", stillSource).attr("data-animate", animatedSource).attr("data-state", "still");
+            foodDiv.append(foodImage);
 
             var rating = results[i].rating;
             var p = $("<p>")
             p.text("Rating: " + rating.toUpperCase());
+            foodDiv.append(p);
 
-            $("#food-view").prepend(foodImage, p);
+            foodDiv.addClass("foodDiv");
+            $("#food-view").prepend(foodDiv);
         };
-
-        // Ability to pause/animate gif on click
-        $(".gif").on("click", function () {
-
-            var still = $(this).attr("data-still");
-            var animate = $(this).attr("data-animate");
-            var state = $(this).attr("data-state");
-
-            if (state === "still") {
-                $(this).attr("src", animate).attr("data-state", "animate");
-            }
-            else {
-                $(this).attr("src", still).attr("data-state", "still");
-            }
-        });
     });
+};
+
+// Ability to pause/animate gif on click
+function changeState() {
+
+    var still = $(this).attr("data-still");
+    var animate = $(this).attr("data-animate");
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", animate).attr("data-state", "animate");
+    }
+    else {
+        $(this).attr("src", still).attr("data-state", "still");
+    }
 };
